a9f2eb7abf441d5ae3b70420346afd3b2fdc1f6e
Ritikjainkkar <ritikjainkkar@gmail.com>
2023-02-19 18:42:58 +0530

    feat: project set up

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+const env = {}
+
+module.exports = {
+  nextConfig: nextConfig,
+  env: {
+    ...env
+  }
+}
